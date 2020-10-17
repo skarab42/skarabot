7d9d3358ad697f83a8966507c868901e27134a0d
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-10-17 07:31:14 +0200

    split code: extract the config

---
+++ server/config.js
@@ -0,0 +1,18 @@
+const port = 4224;
+const host = "localhost";
+const address = `${host}:${port}`;
+
+module.exports = {
+  server: {
+    host,
+    port,
+    address,
+    publicPath: "public"
+  },
+  twitch: {
+    clientId: "i1zqws5ibrt0vdaiuenhcpoe4t5rnb",
+    redirectURI: `http://${address}/token`,
+    forceVerify: false,
+    channels: ["skarab42"]
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,31 +1,21 @@
 const { name, version } = require("../package.json");
 const TwitchClient = require("./twitch/Client");
+const config = require("./config");
 const io = require("socket.io");
 const polka = require("polka");
 const sirv = require("sirv");
 
-const port = 4224;
-const host = "localhost";
-const address = `${host}:${port}`;
-const public = sirv("public", { dev: true });
-
-const twitchConfig = {
-  clientId: "i1zqws5ibrt0vdaiuenhcpoe4t5rnb",
-  redirectURI: `http://${address}/token`,
-  forceVerify: false,
-  channels: ["fablab_onlfait"]
-};
-
-const twitchClient = new TwitchClient(twitchConfig);
+const twitchClient = new TwitchClient(config.twitch);
 const twitchAuth = twitchClient.auth.bind(twitchClient);
+const public = sirv(config.server.publicPath, { dev: true });
 
 const { server } = polka()
   .use(public)
   .use(twitchAuth)
-  .listen(port, async err => {
+  .listen(config.server.port, async err => {
     if (err) throw err;
     console.log(`> ${name} v${version}`);
-    console.log(`> running on ${address}`);
+    console.log(`> running on ${config.server.address}`);
   });
 
 twitchClient.setSocketIO(io(server));
