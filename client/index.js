ae651ab7ba684ce5deb631f39e95be00986e6662
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-10-18 16:20:36 +0200

    handle onMessage event

--- client/index.js
+++ client/index.js
@@ -1,23 +1,5 @@
 const socket = require("socket.io-client")();
 
-const userIds = [];
-
-socket.on("twitch.chat.onMessage", ({ channel, user, message, data, msg }) => {
-  const userId = msg._tags["user-id"];
-  console.log(data);
-  if (!userIds.includes(userId)) {
-    console.log("add user:", userId);
-    userIds.push(userId);
-    socket.emit(
-      "twitch.api",
-      "users",
-      "getUserById",
-      userId,
-      ({ data, error }) => {
-        const profileURL = data["profile_image_url"];
-        const displayName = data["display_name"];
-        console.log(displayName, profileURL);
-      }
-    );
-  }
+socket.on("twitch.chat.onMessage", chatMessage => {
+  console.log("onMessage:", chatMessage);
 });
