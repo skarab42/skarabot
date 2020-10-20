457601e1550da89025342bffa6a99f3f0c7bc5e6
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-10-20 08:05:29 +0200

    set start time to zero when offline

--- server/twitch/plugins/on-message/start-time.js
+++ server/twitch/plugins/on-message/start-time.js
@@ -8,8 +8,13 @@ module.exports = async ({ message, client }, next) => {
 
   const channel = message.channel.slice(1);
   const stream = await client.api.helix.streams.getStreamByUserName(channel);
-  const startedAt = stream._data["started_at"];
 
+  if (!stream) {
+    message.data.startTime = 0;
+    return next();
+  }
+
+  const startedAt = stream._data["started_at"];
   const gmt = Math.abs(new Date().getTimezoneOffset()) * 60;
   startTime = new Date(startedAt).getTime() + gmt;
   message.data.startTime = startTime;
