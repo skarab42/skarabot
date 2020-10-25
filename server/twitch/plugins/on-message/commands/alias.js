88ffa295f706308a179204f27447a16a9d32651e
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-10-25 11:38:52 +0100

    moved alias to config

--- server/twitch/plugins/on-message/commands/alias.js
+++ server/twitch/plugins/on-message/commands/alias.js
@@ -1,8 +1,4 @@
-const alias = {
-  discord: `https://discord.com/invite/efXKCS`,
-  onlfait: `https://twitch.tv/fablab_onlfait`,
-  git: `https://github.com/skarab42 || https://github.com/onlfait`
-};
+const alias = require("../config/alias");
 
 module.exports = ({ command, message, client }) => {
   const text = alias[command.args[0]];
---
+++ server/twitch/plugins/on-message/config/alias.js
@@ -0,0 +1,6 @@
+module.exports = {
+  discord: `https://discord.com/invite/efXKCS`,
+  onlfait: `https://twitch.tv/fablab_onlfait`,
+  fablab: `https://www.fablabs.io/labs/map`,
+  git: `https://github.com/skarab42 || https://github.com/onlfait || https://github.com/lautr3k`
+};
