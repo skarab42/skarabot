74982119af4579cdf27080cd5dd1a475d1c5fdee
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-11-14 10:54:29 +0100

    add user avatar

--- server/twitch/plugins/on-message/ideas.js
+++ server/twitch/plugins/on-message/ideas.js
@@ -1,11 +1,12 @@
 const logs = require("../../../libs/logs");
 
 module.exports = ({ message, client }, next) => {
+  const { avatarURL } = message.data.user;
   const text = message.message;
   const { user } = message;
 
   if (text.startsWith("!!!") || text.endsWith("!!!")) {
-    logs.add("idea", { user, text });
+    logs.add("idea", { user, avatarURL, text });
     client.io.emit("logs.update", logs.getAll());
   }
 
--- server/twitch/plugins/on-message/questions.js
+++ server/twitch/plugins/on-message/questions.js
@@ -1,13 +1,14 @@
 const logs = require("../../../libs/logs");
 
 module.exports = ({ message, client }, next) => {
+  const { avatarURL } = message.data.user;
   const text = message.message;
   const { user } = message;
 
   const isIdea = text.startsWith("!!!") || text.endsWith("!!!");
 
   if (text.includes("???") && !isIdea) {
-    logs.add("question", { user, text });
+    logs.add("question", { user, avatarURL, text });
     client.io.emit("logs.update", logs.getAll());
   }
 
