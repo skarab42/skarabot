8e8b444031e444340088ed7f90effa48a8c10340
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-11-14 11:29:16 +0100

    add help to show playlist

--- server/twitch/plugins/on-message/commands/play.js
+++ server/twitch/plugins/on-message/commands/play.js
@@ -12,12 +12,18 @@ module.exports = ({ command, message, client }) => {
     return;
   }
 
-  const elapsed = message.data.timestamp - lastPlayTime;
+  const fileList = fs.readdirSync(downloadDir);
+
+  if (name === "-h") {
+    const playlist = fileList.map((f) => f.slice(0, -4)).join(" | ");
+    client.chat.say(message.channel, `Playlist: ${playlist}`);
+    return;
+  }
 
   let extactMatch = null;
   let partialMatch = [];
 
-  fs.readdirSync(downloadDir).forEach((file) => {
+  fileList.forEach((file) => {
     if (file === name) {
       extactMatch = file;
     } else if (file.match(new RegExp(`${name}.[a-z0-9]{3}`))) {
@@ -31,6 +37,8 @@ module.exports = ({ command, message, client }) => {
     client.chat.say(message.channel, `Aucun fichier trouvé pour ${name}...`);
     return;
   }
+
+  const elapsed = message.data.timestamp - lastPlayTime;
 
   if (elapsed < playCooldown) {
     const rest = parseInt((playCooldown - elapsed) / 1000);
